c2234c8698518692979ed3d82746ac55e3571973
Aaron Giordano-Barry <opeologist@gmail.com>
2023-07-11 19:19:57 -0400

    use PR version

--- __tests__/ui/Ground.test.tsx
+++ __tests__/ui/Ground.test.tsx
@@ -1,10 +1,16 @@
 import { Ground } from "@/ui/Ground";
-import { create } from "@react-three/test-renderer";
+import { act, create, waitFor } from "@react-three/test-renderer";
+import { Suspense } from "react";
 
 describe("Ground", () => {
   it("renders", async () => {
-    const renderer = await create(<Ground />);
+    const renderer = await create(
+      <Suspense>
+        <Ground />
+      </Suspense>
+    );
 
+    await waitFor(() => expect(renderer.scene.children[0]).toBeDefined());
     console.log(renderer.scene.children);
   });
 });
